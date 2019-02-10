bb37d4f819e923973c6f20a8a927bd2f36f04798
MelanieMenard <Aline1943>
2019-02-10 23:15:21 +0000

    Customise react-scripts with eslint, tslint and absolute paths

--- packages/react-scripts/scripts/utils/createJestConfig.js
+++ packages/react-scripts/scripts/utils/createJestConfig.js
@@ -8,7 +8,7 @@
 'use strict';
 
 const fs = require('fs');
-const chalk = require('chalk');
+const chalk = require('react-dev-utils/chalk');
 const paths = require('../../config/paths');
 
 module.exports = (resolve, rootDir, isEjecting) => {
@@ -60,6 +60,9 @@ module.exports = (resolve, rootDir, isEjecting) => {
     moduleNameMapper: {
       '^react-native$': 'react-native-web',
       '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+      // mmenard-react-scripts BEGIN : Allow jest to resolve the webpack alias for AppSrc
+      '^AppSrc(.*)$': '<rootDir>/src$1',
+      // mmenard-react-scripts END
     },
     moduleFileExtensions: [...paths.moduleFileExtensions, 'node'].filter(
       ext => !ext.includes('mjs')
